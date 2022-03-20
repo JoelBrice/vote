0ddfcea4676aed382209aa849847596e6c1c1139
Joel Tiogo <tiogojoel@gmail.com>
2022-03-20 23:55:19 +0200

    css modified.

--- script/script.js
+++ script/script.js
@@ -1,1 +1,10 @@
-var backendCanvas =  getElementById("backendCanvas").getContext("2d");
+var backendCanvas = getElementById("backendCanvas").getContext("2d");
+var paragraphs = document.getElementsByTagName("p");
+for (var i = 0; i < paragraphs.length; i++) {
+    var paragraph = paragraphs.item(i);
+    paragraph.style.setProperty("color", "blue", null);
+}
+
+d3.selectAll(".backend").style("color", function () {
+    return "hsl(" + Math.random() * 360 + ",100%,50%)";
+});
